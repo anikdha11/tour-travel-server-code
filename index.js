c730838f38e4894c6e9b4dc270914e1237a6ddfe
anikdhar11 <aniksutradhar11@gmail.com>
2021-10-31 15:47:33 +0600

    single add

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 const cors = require('cors');
+const objectId = require('mongodb').ObjectId;
 
 
 const app = express();
@@ -14,25 +15,33 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gt16r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-async function run(){
+async function run() {
 
-    try{
+    try {
         await client.connect();
-         const database = client.db('tour_detail');
-         const packageCollection = database.collection('packages');
-         //Get Api
-         app.get('/packages', async(req,res)=>{
-             const cursor = packageCollection.find({});
-             const packages = await cursor.toArray();
-             res.send(packages);
-         })
-       
+        const database = client.db('tour_detail');
+        const packageCollection = database.collection('packages');
+        //Get Api
+        app.get('/packages', async (req, res) => {
+            const cursor = packageCollection.find({});
+            const packages = await cursor.toArray();
+            res.send(packages);
+        })
+        //Get single package
+        app.get('/packages/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: objectId(id) };
+            const package = await packageCollection.findOne(query);
+            res.json(package);
+
+        })
+
     }
 
 
-finally{
-    // await client.close();
-}
+    finally {
+        // await client.close();
+    }
 
 
 }
@@ -43,10 +52,10 @@ run().catch(console.dir);
 
 
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('Tour server is running ohh okk ');
 })
 
-app.listen(port,()=>{
-    console.log('server is running at port',port);
+app.listen(port, () => {
+    console.log('server is running at port', port);
 })
